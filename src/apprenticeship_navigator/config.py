a0f62f25a5f14ef9047b14473aff00c78c45c59e
Stuart Clark <stuartclarkfc@gmail.com>
2026-09-07 13:10:55 +0500

    Require apprenticeship_api_key, fix mypy.ini formatting

--- src/apprenticeship_navigator/config.py
+++ src/apprenticeship_navigator/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    apprenticeship_api_key: str = ""
+    apprenticeship_api_key: str
     app_env: str = "development"
     log_level: str = "debug"
 
